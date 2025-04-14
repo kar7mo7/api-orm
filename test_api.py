d98c3081da9903afd944c99806f00734eb9f49c8
Jaloliddin <kjaloliddin21@gmail.com>
2025-04-14 09:16:27 +0500

    Initial commit

---
+++ test_api.py
@@ -0,0 +1,11 @@
+from main import app
+from starlette.testclient import TestClient
+
+client = TestClient(app)
+
+
+def test_read_user():
+    response = client.get("/api/user")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello User!"}
+
